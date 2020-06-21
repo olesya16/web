c77eb144747dd51a697be467f306651020baf7ef
web <olesya.rom15@gmail.com>
2020-06-22 01:47:43 +0800

    Работа закончена

---
+++ resources/views/index.blade.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Косметический массаж тела</title>
+	<link rel="stylesheet" href="css/main.css">
+</head>
+<body class="">
+	<header id="home">
+		<div class="container-fluid">
+			<div class="container">
+				<div class="glavnaya">
+					<p>Главная</p>
+					
+				</div>
+				<div class="vidi-uslug">
+					<p>Услуги</p>
+					</a>
+				</div>
+				<div class="mesto">
+					<p>Как нас найти</p>
+				</div>
+				<div class="kontakty">
+					<p>Контакты</p>
+				</div>
+				<div class="row">
+					<div class="col1 col2">
+						<a href="#" class="thumbnail">
+							<img src="img/logo.png" alt="" class="img-responsive">
+						</a>
+					</div>
+                </div>
+				<div class="jumbotron">
+					<h1><strong>Массажный салон "Нежность"</strong></br>
+					<small>Окунись в удовольствие!</small></h1>
+				</div>
+			</div>
+			<div class="slogan">
+			<p>
+			Лучший и единственный массажный салон </br>
+			в маленьком и уютном городке Саянск</br>
+			В месяц к нам обращаются около одной тысячи человек</br>
+			Мы помогаем расслабиться после тяжелого трудового дня</br>
+			или в любое удобное для Вас время</p>
+			</div>
+		</div>
+	</header>
+	<section>
+	<div class="section1">
+		<div class="fonosnov">
+			<div class="girl">
+			</div>
+				<div class="fonbel">
+					<div class="vvodnyi">
+			<p>
+			Массаж - одна и самых популярных услуг, представляет собой целый</br>
+			комплекс действий, которые направлены на оздоровление и расслабление</br>
+			всего тела. Во время массажа из организма выводятся шлаки и токсины,</br>
+			а также активизируются потоки крови. После массажа тело приходит в тонус и</br>
+			улучшается общее самочувствие.</br>
+			</br>
+			Массаж снимает стресс, а также боли в спине и других частях тела, дает</br>
+			прилив сил и бодрости. Многие спортсмены используют массаж для</br>
+			расслабления и восстановления мышц после тренировок. Кроме того массаж</br>
+			повышает работоспособность. В дополнение ко всему, массаж это еще и</br>
+			очень приятная процедура.</br>
+			</br>
+			Если Вам нужен профессиональный массаж, то смело обращайтесь в наш</br>
+			салон. Опытный мастер поможет расслабится и принесет пользу Вашему</br>
+			организму.
+			</p>
+					</div>
+				</div>
+		</div>
+	</div>
+	<div class="brownpolosa">
+		<div class="price-list">
+			<p>Виды услуг</p>
+		</div>
+	</div>
+	</section>
+	<section>
+	<div class="price-fon">
+	<div class="price">
+		<p>ОБЩИЙ КЛАССИЧЕСКИЙ МАССАЖ (1 ЧАС)<span class="price2">1200 РУБ.</span></p>
+		<p>МАССАЖ RELAX (1 ЧАС)<span class="price2">1200 РУБ.</span></p>
+		<p>МОДЕЛИРУЮЩИЙ МАССАЖ (1 ЧАС)<span class="price2">1200 РУБ.</span></p>
+		<p>АНТИЦЕЛЛЮЛИТНЫЙ МАССАЖ (1 ЧАС)<span class="price2">1200 РУБ.</span></p>
+		<p>ЛИМФОДРЕНАЖНЫЙ МАССАЖ (45 МИН)<span class="price2">800 РУБ.</span></p>
+		<p>МАССАЖ ШЕЙНО-ВОРОТНИКОВОЙ ЗОНЫ (20 МИН)<span class="price2">500 РУБ.</span></p>
+		<p>МАССАЖ СПИНЫ (30 МИН)<span class="price2">500 РУБ.</span></p>
+		<p>МАССАЖ НОГ, СТУПНЕЙ (25 МИН)<span class="price2">450 РУБ.</span></p>
+		<p>МАССАЖ РУК (20 МИН)<span class="price2">300 РУБ.</span></p>
+		<p>МАССАЖ ГОЛОВЫ (10 МИН)<span class="price2">300 РУБ.</span></p>
+	</div>
+	</div>
+	</section>
+	<section>
+		<div class="brownpolosa">
+			<div class="price-list">
+			<p>Наш адрес</p>
+			</div>
+		</div>
+	</section>
+	<section>
+	<div class="price-fon">
+		<div class="adress">
+			<p>Мы находимся по адресу: Массажный салон "Нежность"</br>
+			</br>
+			г. Саянск, мкр. Юбилейный, д. 12, 1 этаж</p>
+		</div>
+	</div>
+	</section>
+	<section>
+		<div class="brownpolosa">
+			<div class="price-list">
+			<p>Контакты</p>
+			</div>
+		</div>
+	</section>
+	<footer>
+		<div class="price-fon">
+			<div class="svyaz">
+			<p>ПОЧТА:<span class="price2">nezhnostsayansk@mail.ru</span></p></p>
+			<p>INSTAGRAM:<span class="price2">nezhnostSayansk</span></p></p>
+			<p>Вконтакте:<span class="price2">nezhnostmassage</span></p></p>
+			<p>Facebook:<span class="price2">nezhnostmassage1</span></p></p>
+			<p>Twitter:<span class="price2">nezhnostmassageSayansk</span></p></p>
+			</div>
+	</footer>
+</body>
+</html>
